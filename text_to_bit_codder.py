a46784e0839baee987e248e2350858f5690ebfa1
Jstrykow <jakub@strykowski.eu>
2022-04-02 17:56:24 +0200

    Polisch characters to bin

--- text_to_bit_codder.py
+++ text_to_bit_codder.py
@@ -3,7 +3,7 @@ with open("antygona.txt",encoding="utf8") as f:
     for line in f:
         antygona += line
 
-byte_antygona = bytes(antygona, 'utf-8')
-print(byte_antygona)
+for c in antygona:
+    print(f"{c}, {bin(ord(c))}")
 
 # dobra to akcja jest taka, że na dwóch bajtach zapisywana jest utf-8 i musimy dwa baity przerzicać dla jednego znaku
