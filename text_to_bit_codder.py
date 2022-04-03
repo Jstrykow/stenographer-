6847e8933c85d1bf5a4bd9e3f7d4fac1494d684c
Jstrykow <jakub@strykowski.eu>
2022-04-03 12:47:11 +0200

    UTF-8 coding polish characters, big utf -8 16 bi& C:/Users/Administrator/AppData/Local/Programs/Python/Python310/python.exe

--- text_to_bit_codder.py
+++ text_to_bit_codder.py
@@ -3,7 +3,16 @@ with open("antygona.txt",encoding="utf8") as f:
     for line in f:
         antygona += line
 
-for c in antygona:
-    print(f"{c}, {bin(ord(c))}")
+# https://stackoverflow.com/questions/7396849/convert-binary-to-ascii-and-vice-versa
+def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+    bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-# dobra to akcja jest taka, że na dwóch bajtach zapisywana jest utf-8 i musimy dwa baity przerzicać dla jednego znaku
+def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+    n = int(bits, 2)
+    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
+
+
+bits = text_to_bits(antygona)
+print(bits)
+print(text_from_bits(bits))
